70163b404cb112bed417c3b457c0f49ab7802726
Nikolas <niko.owen88@gmail.com>
2024-04-20 21:27:19 +0700

    new syntax

--- UFO/ufoAlien.js
+++ UFO/ufoAlien.js
@@ -149,31 +149,44 @@ function main() {
   GL.enableVertexAttribArray(_position);
   GL.useProgram(SHADER_PROGRAM);
 
-  // 
-  // Kepala
-  //
-  var halfSphereData = generateHalfSphere(0, 0, 2.5, 1.5, 30, [0, 1, 1]);
+  // ========================== Kepala ==================================
+  //bagian dalam
+  var kepala1 = generateHalfSphere(0, 0, 2.5, 1.5, 30, [0, 0.45, 0.64]);
 
   // Create buffers
-  var halfSphere_vertex = createVertexBuffer(GL, halfSphereData.vertices);
-  var halfSphere_colors = createColorBuffer(GL, halfSphereData.colors);
-  var halfSphere_faces = createFacesBuffer(GL, halfSphereData.faces);
+  var kepala1_vertex = createVertexBuffer(GL, kepala1.vertices);
+  var kepala1_colors = createColorBuffer(GL, kepala1.colors);
+  var kepala1_faces = createFacesBuffer(GL, kepala1.faces);
+
+  //bagian pola
+  var kepala2 = generateHalfSphere(0, 0, 2.9, 1.53, 25, [0.11, 0.88, 0.94]);
+
+  // Create buffers
+  var kepala2_vertex = createVertexBuffer(GL, kepala2.vertices);
+  var kepala2_colors = createColorBuffer(GL, kepala2.colors);
+  var kepala2_faces = createFacesBuffer(GL, kepala2.faces);
+  
   
 
-  //
-  // Badan
-  //
-  var tubeData = generateSolidTube(0, 0, 1, 1.5, 1, 3, 30, [0, 1, 1]);
+  // ========================== Badan ==================================
+  // bagian luar
+  var badan1 = generateSolidTube(0, 0, 1, 1.5, 1, 3, 30, [0, 0.45, 0.64]);
 
   // Create buffers for the first octagon
-  var SOLID_TUBES_VERTEX = createVertexBuffer(GL, tubeData.vertices);
-  var SOLID_TUBES_COLORS = createColorBuffer(GL, tubeData.colors);
-  var SOLID_TUBES_FACES = createFacesBuffer(GL, tubeData.faces);
+  var badan1_VERTEX = createVertexBuffer(GL, badan1.vertices);
+  var badan1_COLORS = createColorBuffer(GL, badan1.colors);
+  var badan1_FACES = createFacesBuffer(GL, badan1.faces);
+
+  // bagian dalam
+  var badan2 = generateSolidTube(0, 0, 1, 1, 1.6, 3.2, 7, [0.11, 0.88, 0.94]);
+
+  // Create buffers for the first octagon
+  var badan2_VERTEX = createVertexBuffer(GL, badan2.vertices);
+  var badan2_COLORS = createColorBuffer(GL, badan2.colors);
+  var badan2_FACES = createFacesBuffer(GL, badan2.faces);
 
 
-  //
-  //Tube backpack 
-  //
+  // ========================== Tube Backpack ==================================
   var tubeData2 = generateSolidTube(2.2, 0, 1, 0.65, 0.4, 3, 30, [0.4, 0.4, 0.4]);
 
   // Create buffers
@@ -182,14 +195,11 @@ function main() {
   var SOLID_TUBES2_FACES = createFacesBuffer(GL, tubeData2.faces);
 
 
+  // ========================== Mata ==================================
+  
+  
 
-  //
-  // Mata
-  //
-
-  //
-  // UFO
-  //
+  // ========================== UFO ==================================
 
   // UFO1
   var ufo1 = generateUfoOutline(2.5, 2.5, 1.2, 40);
@@ -248,14 +258,14 @@ function main() {
     LIBS.rotateY(MODEL_MATRIX, theta);
     LIBS.rotateX(MODEL_MATRIX, alpha);
 
-    //gambar ellip kepala
-    GL.bindBuffer(GL.ARRAY_BUFFER, halfSphere_vertex);
+    // kepala1
+    GL.bindBuffer(GL.ARRAY_BUFFER, kepala1_vertex);
     GL.vertexAttribPointer(_position, 3, GL.FLOAT, false, 0, 0);
 
-    GL.bindBuffer(GL.ARRAY_BUFFER, halfSphere_colors);
+    GL.bindBuffer(GL.ARRAY_BUFFER, kepala1_colors);
     GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 0, 0);
 
-    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, halfSphere_faces);
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, kepala1_faces);
 
     GL.uniformMatrix4fv(_PMatrix, false, PROJECTION_MATRIX);
     GL.uniformMatrix4fv(_VMatrix, false, VIEW_MATRIX);
@@ -263,19 +273,39 @@ function main() {
 
     GL.drawElements(
       GL.TRIANGLE_STRIP,
-      halfSphereData.faces.length,
+      kepala1.faces.length,
       GL.UNSIGNED_SHORT,
       0
     );
 
-    // Draw tuba untuk badan
-    GL.bindBuffer(GL.ARRAY_BUFFER, SOLID_TUBES_VERTEX);
+    // kepala2
+    GL.bindBuffer(GL.ARRAY_BUFFER, kepala2_vertex);
     GL.vertexAttribPointer(_position, 3, GL.FLOAT, false, 0, 0);
 
-    GL.bindBuffer(GL.ARRAY_BUFFER, SOLID_TUBES_COLORS);
+    GL.bindBuffer(GL.ARRAY_BUFFER, kepala2_colors);
     GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 0, 0);
 
-    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, SOLID_TUBES_FACES);
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, kepala2_faces);
+
+    GL.uniformMatrix4fv(_PMatrix, false, PROJECTION_MATRIX);
+    GL.uniformMatrix4fv(_VMatrix, false, VIEW_MATRIX);
+    GL.uniformMatrix4fv(_MMatrix, false, MODEL_MATRIX);
+
+    GL.drawElements(
+      GL.TRIANGLES,
+      kepala2.faces.length,
+      GL.UNSIGNED_SHORT,
+      0
+    );
+
+    // badan 1
+    GL.bindBuffer(GL.ARRAY_BUFFER, badan1_VERTEX);
+    GL.vertexAttribPointer(_position, 3, GL.FLOAT, false, 0, 0);
+
+    GL.bindBuffer(GL.ARRAY_BUFFER, badan1_COLORS);
+    GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 0, 0);
+
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, badan1_FACES);
 
     GL.uniformMatrix4fv(_PMatrix, false, PROJECTION_MATRIX);
     GL.uniformMatrix4fv(_VMatrix, false, VIEW_MATRIX);
@@ -283,7 +313,27 @@ function main() {
 
     GL.drawElements(
       GL.TRIANGLE_STRIP,
-      tubeData.faces.length,
+      badan1.faces.length,
+      GL.UNSIGNED_SHORT,
+      0
+    );
+
+    // badan 2
+    GL.bindBuffer(GL.ARRAY_BUFFER, badan2_VERTEX);
+    GL.vertexAttribPointer(_position, 3, GL.FLOAT, false, 0, 0);
+
+    GL.bindBuffer(GL.ARRAY_BUFFER, badan2_COLORS);
+    GL.vertexAttribPointer(_color, 3, GL.FLOAT, false, 0, 0);
+
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, badan2_FACES);
+
+    GL.uniformMatrix4fv(_PMatrix, false, PROJECTION_MATRIX);
+    GL.uniformMatrix4fv(_VMatrix, false, VIEW_MATRIX);
+    GL.uniformMatrix4fv(_MMatrix, false, MODEL_MATRIX);
+
+    GL.drawElements(
+      GL.TRIANGLE_STRIP,
+      badan2.faces.length,
       GL.UNSIGNED_SHORT,
       0
     );
